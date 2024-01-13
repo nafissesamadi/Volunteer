8cc6305a68aedb1605640f9a3c7a178a61579fe9
nafisse <nafissesamadi@yahoo.com>
2024-01-13 13:42:27 +0330

    new design of models

---
+++ application/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+admin.site.register(models.Grade)
+admin.site.register(models.Major)
+admin.site.register(models.CourseName)
+admin.site.register(models.EducationalLevel)
+admin.site.register(models.Course)
+admin.site.register(models.WeekDay)
+admin.site.register(models.AvailableTime)
+admin.site.register(models.SkillType)
+admin.site.register(models.Skill)
+admin.site.register(models.SchoolType)
+admin.site.register(models.School)
+admin.site.register(models.VenueType)
+admin.site.register(models.ClassVenue)
+admin.site.register(models.EducationalVolunteer)
+admin.site.register(models.SkilledVolunteer)
+admin.site.register(models.Student)
+admin.site.register(models.Application)
+admin.site.register(models.AcceptedApplication)
